d509a408be942d450670a714e5b8761e7fe8323c
itsxSAJO <118242127+itsxSAJO@users.noreply.github.com>
2023-02-28 00:17:08 -0500

    Update Vuelo.java
    Pull - Anthony Morales (TonguitaXd) y Said Luna (itsxSAJO)

--- src/GUI/Vuelo.java
+++ src/GUI/Vuelo.java
@@ -48,13 +48,68 @@ public class Vuelo extends JFrame {
         JLabel lblNewLabel_2 = new JLabel("AQUI VA LA BUSQUEDA");
         panelBusqueda.add(lblNewLabel_2);
 
+        /*
+         * Panel del grupo 4 - Anthony Morales y Said Luna
+         */
         JPanel panelGRUPO4 = new JPanel();
         panelGRUPO4.setOpaque(false);
         getContentPane().add(panelGRUPO4, BorderLayout.CENTER);
 
-        JLabel lblNewLabel = new JLabel("PARTE DEL GRUPO 4");
+        JLabel lblNewLabel = new JLabel("INFORMACION DE VUELOS");
         lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
         panelGRUPO4.add(lblNewLabel);
+
+        //****************************************************************** */
+        JButton btnImprimir = new JButton("Imprimir");
+        btnImprimir.addActionListener(new ActionListener() {
+    public void actionPerformed(ActionEvent e) {
+        // Obtenemos los valores seleccionados en los filtros
+        String tipoViaje = (String) cbxConfirmarRetorno.getSelectedItem();
+        String plan = (String) elegirPlan.getSelectedItem();
+        int numPasajeros = Integer.parseInt((String) cantidadPasajeros.getSelectedItem());
+        boolean usarMillas = ckbxUsarMillas.isSelected();
+        String origen = txtOrigen.getText();
+        String destino = txtDestino.getText();
+        String origenIdaVuelta = txtOrigenIdaVuelta.getText();
+        String destinoIdaVuelta = txtDestinoIdaVuelta.getText();
+        String fecha = txtFechaIda.getText();
+        String fechaIdaV = txtFechaIdaV.getText();
+        String fechaVuelta = txtFechaVuelta.getText();
+
+        // Obtener el elemento seleccionado del JComboBox
+        String seleccion = (String) cbxConfirmarRetorno.getSelectedItem();
+        
+        // Cambiar el panel de abajo según el elemento seleccionado
+        if (seleccion.equals("Solo ida")) {
+            String mensaje = "Tipo de viaje: " + tipoViaje + "\n"
+                       + "Plan: " + plan + "\n"
+                       + "Número de pasajeros: " + numPasajeros + "\n"
+                       + "Usar millas: " + usarMillas + "\n"
+                       + "Origen: " + origen + "\n"
+                       + "Destino: " + destino + "\n"
+                       + "Fecha Ida: " + fecha + "\n";
+                       //+ "Fecha Vuelta: " + fechaV + "\n";
+
+        // Mostramos la ventana de diálogo
+        JOptionPane.showMessageDialog(Vuelo.this, mensaje, "Imprimir datos", JOptionPane.INFORMATION_MESSAGE);
+        } else if (seleccion.equals("Ida y Vuelta")) {
+            String mensaje = "Tipo de viaje: " + tipoViaje + "\n"
+                       + "Plan: " + plan + "\n"
+                       + "Número de pasajeros: " + numPasajeros + "\n"
+                       + "Usar millas: " + usarMillas + "\n"
+                       + "Origen: " + origenIdaVuelta + "\n"
+                       + "Destino: " + destinoIdaVuelta + "\n"
+                       + "Fecha Ida: " + fechaIdaV + "\n"
+                       + "Fecha Vuelta: " + fechaVuelta + "\n";
+
+        // Mostramos la ventana de diálogo
+        JOptionPane.showMessageDialog(Vuelo.this, mensaje, "Imprimir datos", JOptionPane.INFORMATION_MESSAGE);
+        }
+        // Creamos el mensaje que se mostrará en la ventana de impresión
+    }
+});
+panelBusqueda.add(btnImprimir, BorderLayout.EAST);
+    }
 
     }
 

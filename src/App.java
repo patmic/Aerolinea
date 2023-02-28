f930395f39b4ab8ff6a193d1244f942184500ac0
dakookie <damaris.suquillo@epn.edu.ec>
2023-02-27 21:15:47 -0500

    Prueba DS

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, aerolinea of pat_mic......!");
+        System.out.println("Hi, DS");
     }
 }

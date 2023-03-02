1c7ac7ec22c7f26c11ed842ec68af21c87baad88
Alejololer <acarlos29112003@gmail.com>
2023-03-02 18:55:17 -0500

    commit

--- src/App.java
+++ src/App.java
@@ -1,5 +1,30 @@
+import BL.VueloBL;
+import BL.Entities.Vuelo;
+import FWK.AppConfiguration;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, aerolinea of pat_mic......!");
+        AppConfiguration.load("src/appConfig.properties");
+
+        try {
+            VueloBL vuelo = new VueloBL();
+            for (Vuelo v : vuelo.getAllVuelo()) {
+                System.out.println("Id: " + v.getId());
+                System.out.println("Origen: " + v.getOrigen());
+                System.out.println("Destino: " + v.getDestino());
+                System.out.println("Fecha: " + v.getFecha());
+                System.out.println("Clase: " + v.getClaseVuelo());
+                System.out.println("Hora Salida: " + v.getHoraSalida());
+                System.out.println("Hora Llegada: " + v.getHoraLlegada());
+                System.out.println("Precio: " + v.getPrecio());
+                System.out.println("Equipaje: ");
+                for (String string : v.getEquipajePermitido()) {
+                    System.out.println("\t- " + string);
+                }
+                System.out.println("-----------------------------------------------");
+            }
+        } catch (Exception e) {
+            // TODO
+        }
     }
 }
